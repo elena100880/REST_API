4335c7d4315e0f3e5df5d819481abaad843f3126
elena100880 <100880kr@gmail.com>
2021-06-23 22:20:08 +0200

    add validation

--- src/Controller/ProductInStoreController.php
+++ src/Controller/ProductInStoreController.php
@@ -15,8 +15,6 @@ use Doctrine\ORM\EntityManagerInterface;
 
 use Doctrine\ORM\Tools\Pagination\Paginator;
 
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-
 class ProductInStoreController extends AbstractController
 {    
     public function product_list(Request $request): Response  
@@ -32,10 +30,10 @@ class ProductInStoreController extends AbstractController
             $amount = $request->query->get('amount') ?? 1;
             
             $page = ($request->query->get('page')) ?? 1;
-            if (!$this->if_string_is_natural_number($page)) throw new Exception ("Invalid value of PAGE");
+            $this->if_string_is_natural_number($page);
             
             $elements = $request->query->get('elements') ?? 2;
-            if ( !$this->if_string_is_natural_number($elements) or $elements > 1000) throw new Exception ("Invalid value of ELEMENTS");
+            $this->is_elements_valid($elements);
         }
         catch (Exception $e) {return new Response ($e->getMessage(), Response::HTTP_BAD_REQUEST);}
         
@@ -50,11 +48,15 @@ class ProductInStoreController extends AbstractController
         $DQLquery = $em->createQuery($dql)
                                     ->setFirstResult($page * $elements  - $elements)
                                     ->setMaxResults($elements);
-        try {
+
+        try { /** @todo Another way of catching Error here??? */
             $productsPage = new Paginator($DQLquery);
             $totalElements = count($productsPage); 
         }
-        catch (\Exception $e) {return new Response ("DB is not available", Response::HTTP_SERVICE_UNAVAILABLE);}
+        catch (\Exception $e) {
+            $message = $e->getMessage();
+            return new Response ("Developer info: $message<br><br> DB is not available", Response::HTTP_SERVICE_UNAVAILABLE);
+        }
         
         $returnResponse = ["data" => $productsPage, "total" => $totalElements];
         return $this->json($returnResponse);  
@@ -69,18 +71,21 @@ class ProductInStoreController extends AbstractController
                             -> where ('p.id = :id')
                             -> getQuery();
         
-        try {$result = $queryBuilder -> execute(); }               
-        catch (\Exception $e) {return new Response ("DB is not available", Response::HTTP_SERVICE_UNAVAILABLE);}
+        try {$result = $queryBuilder -> execute(); }      /** @todo Another way of catching Error here??? */         
+        catch (\Exception $e) {
+            $message = $e->getMessage();
+            return new Response ("Developer info: $message<br><br> DB is not available", Response::HTTP_SERVICE_UNAVAILABLE);
+        }
 
         if ($result) return new Response ("Success", Response::HTTP_OK);
         else return new Response ("Item NOT FOUND", Response::HTTP_NOT_FOUND);
     }
 
-    public function product_add (Request $request, ValidatorInterface $validator): Response
+    public function product_add (Request $request): Response
     {
         /* required json data from Request:
          * {    
-         *      "name": "Product X", //required
+         *      "name": "Product X", //
          *      "amount": 1,   // 0 for default
          * }
          */
@@ -90,14 +95,14 @@ class ProductInStoreController extends AbstractController
     //validating parameters:
         try 
         {
-            $data = ($this->valid_json($json)) ? json_decode($json, true) : throw new Exception('Invalid json');
+            $this->valid_json($json);
+            $data = json_decode($json, true);
 
-            $name = $data['name'] ?? throw new Exception('Invalid key for NAME');
-            if (!is_string($data['name']) or strlen($data['name']) > 50) throw new Exception('Invalid value for NAME');
-
+            $name = trim($data['name']) ?? null;
+            $this->is_name_valid ($name);
+            
             $amount = $data['amount'] ?? 0;
-            if ( !is_int($amount) or $amount < 0) throw new Exception ("Invalid value of AMOUNT");
-         
+            $this->is_amount_valid ($amount);
         }
         catch (\Exception $e) {return new Response ($e->getMessage(), Response::HTTP_BAD_REQUEST); }
         
@@ -109,8 +114,11 @@ class ProductInStoreController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $em -> persist($product);
         
-        try {$em -> flush();} 
-        catch (\Exception $e) {return new Response ("DB is not available", Response::HTTP_SERVICE_UNAVAILABLE);}
+        try {$em -> flush();} /** @todo Another way of catching Error here??? */
+        catch (\Exception $e) {
+            $message = $e->getMessage();
+            return new Response ("Developer info: $message<br><br> DB is not available", Response::HTTP_SERVICE_UNAVAILABLE);
+        }
 
         return new Response ("Success", Response::HTTP_OK);
     }
@@ -131,18 +139,17 @@ class ProductInStoreController extends AbstractController
         {
             $data = ($this->valid_json($json)) ? json_decode($json, true) : throw new Exception('Invalid json');
 
-            if (isset($data['name'])) $name = $data['name'];
-            if (!is_string($data['name']) or strlen($data['name']) > 50) throw new Exception('Invalid value for NAME');
+            if (isset($data['name'])) $name = trim($data['name']);
+            $this->is_name_valid ($name);
            
             if (isset($data['amount'])) $amount = $data['amount'];
-            if ( !is_int($amount) or $amount < 0) throw new Exception ("Invalid value of AMOUNT");
+            $this->is_amount_valid ($amount);
         }
         catch (Exception $e) {
             return new Response ($e->getMessage(), Response::HTTP_BAD_REQUEST);
         }
 
     //updating product in DB:
-
         $em = $this->getDoctrine()->getManager();
         $queryBuilder = $em -> createQueryBuilder()
                             -> update ('App\Entity\ProductInStore', 'p')
@@ -157,8 +164,11 @@ class ProductInStoreController extends AbstractController
                             -> setParameter ('id', $id)
                             -> getQuery();
                             
-        try {$result = $queryBuilder -> execute(); }               
-        catch (\Exception $e) {return new Response ("DB is not available", Response::HTTP_SERVICE_UNAVAILABLE);}
+        try {$result = $queryBuilder -> execute();}     /** @todo Another way of catching Error here??? */          
+        catch (\Exception $e) {
+            $message = $e->getMessage();
+            return new Response ("Developer info: $message<br><br> DB is not available", Response::HTTP_SERVICE_UNAVAILABLE);
+        }
 
         if ($result) return new Response ("Success", Response::HTTP_OK);
         else return new Response ("Item NOT FOUND", Response::HTTP_NOT_FOUND);
@@ -166,11 +176,36 @@ class ProductInStoreController extends AbstractController
         return new Response ("Success", Response::HTTP_OK);
     }
     
-    private function if_string_is_natural_number ($string) {
-        return (is_numeric($string) and ($string - floor($string) == 0) and $string > 0 );
+    private function valid_json($string)  { 
+        json_decode($string);
+        if (json_last_error() !== 0) throw new Exception('Invalid json');
+        return true;
+    }
+    
+    private function if_string_is_natural_number($string) {
+        if (!is_numeric($string) or ($string - floor($string) != 0) or $string <= 0 ) throw new Exception ("Invalid value for PAGE");
+        return true;
     }
 
-    private function valid_json($string)  { 
-        json_decode($string); return json_last_error() === 0;
+    private function is_elements_valid ($elements) {
+        try {
+            $this->if_string_is_natural_number($elements);
+        }
+        catch (Exception) {
+            throw new Exception ("Invalid value for ELEMENTS");
+        }
+        if ($string > 1000 ) throw new Exception ("Invalid value for ELEMENTS");
+        return true;
     }
+
+    private function is_name_valid ($name) {
+        if ($name === null) throw new Exception('Invalid key for NAME');
+        if (!is_string($name) or strlen($name) > 50 or strlen($name) < 2 or (trim($name) == "") ) throw new Exception('Invalid value for NAME');
+        return true;
+    }
+
+    private function is_amount_valid ($amount) {
+    if ( !is_int($amount) or $amount < 0) throw new Exception ("Invalid value of AMOUNT");
+    return true;
+    }  
 }
